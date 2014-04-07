37cabf3751c7b98b2b05aa9ae43a7567429cd008
zeng83 <zhaofeng.zeng83@gmail.com>
2014-04-07 02:22:10 -0700

    fixed jquery cannot regnize $ issue

--- js/zplayer.js
+++ js/zplayer.js
@@ -2,26 +2,26 @@
 	
 
 	$.fn.zplayer = function(options){
-		var options = $.extend($.fn.zplayer.defaults,options);
+		var setting = $.extend({},$.fn.zplayer.defaults,options);
 
 		return this.each(function(){
 			var $zVideo = $(this);
 
 			var $video_wrap = $('<div></div>').addClass('videoContainer');
 
-			var $video_controls = $('<div class="ghinda-video-controls"><div class="ghinda-video-seek"></div><a class="ghinda-video-play" title="Play/Pause"></a><div class="ghinda-video-timer">00:00</div><div class="ghinda-volume-box"><div class="ghinda-volume-slider"></div><a class="ghinda-volume-button" title="Mute/Unmute"></a></div></div>');
+			var $video_controls = $('<div class="video-controls"><div class="video-seek"></div><a class="video-play" title="Play/Pause"></a><div class="video-timer">00:00</div><div class="volume-box"><div class="volume-slider"></div><a class="volume-button" title="Mute/Unmute"></a></div></div>');
 
 			$zVideo.wrap($video_wrap);
 			$zVideo.after($video_controls);
 
 			//get newly created elements
 			var $video_container = $zVideo.parent('.videoContainer');
-			var $video_controls = $('.ghinda-video-controls', $video_container);
-			var $ghinda_play_btn = $('.ghinda-video-play', $video_container);
-			var $ghinda_video_seek = $('.ghinda-video-seek', $video_container);
-			var $ghinda_video_timer = $('.ghinda-video-timer', $video_container);
-			var $ghinda_volume = $('.ghinda-volume-slider', $video_container);
-			var $ghinda_volume_btn = $('.ghinda-volume-button', $video_container);
+			var $video_controls = $('.video-controls', $video_container);
+			var $play_btn = $('.video-play', $video_container);
+			var $video_seek = $('.video-seek', $video_container);
+			var $video_timer = $('.video-timer', $video_container);
+			var $volume = $('.volume-slider', $video_container);
+			var $volume_btn = $('.volume-button', $video_container);
 
 			$video_controls.hide(); // keep the controls hidden
 		});
